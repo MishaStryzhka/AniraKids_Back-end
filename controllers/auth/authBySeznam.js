970275fb6a162ee440a55af7c61797fc860bf4ef
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-02-23 01:09:27 +0100

    Adds endpoint authBySeznam

--- controllers/auth/authBySeznam.js
+++ controllers/auth/authBySeznam.js
@@ -1,56 +1,41 @@
-// axios
-//   .get('https://login.szn.cz/api/v1/user', { headers })
-//   .then(response => {
-//     // Обработка ответа от API
-//     console.log('Ответ от API:', response.data);
-//   })
-//   .catch(error => {
-//     // Обработка ошибки при запросе
-//     console.error('Ошибка при выполнении запроса:', error);
-//   });
+const { jwtDecode } = require('jwt-decode');
+const { User } = require('../../models');
+const jwt = require('jsonwebtoken');
 
-// const { jwtDecode } = require('jwt-decode');
-// const { User } = require('../../models');
-// const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
 
-// const { SECRET_KEY } = process.env;
+const authBySeznam = async (req, res) => {
+  console.log('req.body', req.body);
 
-// const authBySeznam = async (req, res) => {
-//   const { credential } = req.body;
-//   const decoded = jwtDecode(credential);
+  // eslint-disable-next-line no-unused-vars
+  const { account_name: email } = req.body;
 
-//   // eslint-disable-next-line camelcase
-//   const { email, given_name, family_name, picture } = decoded;
+  let dataUser = await User.findOne({ email });
+  if (!dataUser) {
+    await User.create({
+      email,
+      provider: 'seznam',
+    });
 
-//   let dataUser = await User.findOne({ email });
-//   if (!dataUser) {
-//     await User.create({
-//       email,
-//       firstName: given_name,
-//       lastName: family_name,
-//       avatar: picture,
-//       provider: 'google',
-//     });
+    dataUser = User.findOne({ email });
+  }
 
-//     dataUser = User.findOne({ email });
-//   }
+  const payload = {
+    id: dataUser._id,
+  };
 
-//   const payload = {
-//     id: dataUser._id,
-//   };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
 
-//   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  await User.findByIdAndUpdate(dataUser._id, { token });
 
-//   await User.findByIdAndUpdate(dataUser._id, { token });
+  const { createdAt, updatedAt, ...user } = dataUser._doc;
 
-//   const { createdAt, updatedAt, token: _, ...user } = dataUser._doc;
+  console.log('user', user);
 
-//   console.log('user', user);
+  res.status(201).json({
+    user,
+    token,
+  });
+};
 
-//   res.status(201).json({
-//     user,
-//     token,
-//   });
-// };
-
-// module.exports = authBySeznam;
+module.exports = authBySeznam;
