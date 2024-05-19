d30bb583200b0ab70d13378a5394984bafd6f034
Misha.Strizhka <mykhailo.stryzhka@seznam.cz>
2024-05-19 15:05:47 +0200

    Update getCurrentUser.js

--- controllers/auth/getCurrentUser.js
+++ controllers/auth/getCurrentUser.js
@@ -10,27 +10,8 @@ const getCurrentUser = async (req, res, next) => {
 
   res.status(200).json({
     user: {
+      ...user._doc,
       userID: user._id,
-      isFirstLogin: user.isFirstLogin,
-      avatar: user.avatar,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      nickname: user.nickname,
-
-      favorites: user.favorites,
-
-      primaryPhoneNumber: user.primaryPhoneNumber,
-      primaryPhoneNumberVerified: user.primaryPhoneNumberVerified,
-
-      email: user.email,
-      emailVerified: user.emailVerified,
-
-      provider: user.provider,
-      typeUser: user.typeUser,
-      billingDetails: user.billingDetails,
-      bankAccount: user.bankAccount,
-
-      cart: user.cart,
     },
   });
 };
